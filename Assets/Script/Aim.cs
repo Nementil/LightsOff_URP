79ad8b0d2d13ba9e5033ab22431759fbdf952a66
Mathilde2020 <123827924+Mathilde2020@users.noreply.github.com>
2023-10-21 14:56:29 -0400

    Revert "added controller connexion checking in aim"
    This reverts commit 428723d1a67d2dd7c509b538638d24fed8bfd2ea.

--- Assets/Script/Aim.cs
+++ Assets/Script/Aim.cs
@@ -8,12 +8,9 @@ public class Aim : MonoBehaviour
     [SerializeField] private Camera cam;
     Vector2 mousePos;
     Rigidbody2D rb;
-    private bool connected = false;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(CheckForControllers());
-
     }
     private void Update()
     {
@@ -21,32 +18,8 @@ public class Aim : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Vector2 lookDir = mousePos - rb.position; //connected ? new Vector2(Input.GetAxis("HorizontalRight"), -Input.GetAxis("VerticalRight")) : mousePos - rb.position;
+        Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
-    }
-
-   
-
-    IEnumerator CheckForControllers()
-    {
-        while (true)
-        {
-            var controllers = Input.GetJoystickNames();
-
-            if (!connected && controllers.Length > 0)
-            {
-                connected = true;
-                Debug.Log("Connected");
-
-            }
-            else if (connected && controllers.Length == 0)
-            {
-                connected = false;
-                Debug.Log("Disconnected");
-            }
-
-            yield return new WaitForSeconds(1f);
-        }
     }
 }
