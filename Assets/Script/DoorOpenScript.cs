3d9ba4b985c11481f5f1ec0f03adef93a02a54e1
Tony235235 <TonyFree23@hotmail.com>
2023-10-21 16:29:29 -0400

    TonyVersion

---
+++ Assets/Script/DoorOpenScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenScript : MonoBehaviour
+{
+
+    public class DoorAnimatorParamName = "DoorOpen";
+    
+        
+    private void OnAnimatorIK(int layerIndex)
+    
+        Animator animator; 
+    
+
+         void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+      if(dete)  
+    }
+}
