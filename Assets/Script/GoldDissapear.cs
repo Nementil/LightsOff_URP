c6098671c2d93c3d89cb961c4e643b14033b4f8a
Tony235235 <TonyFree23@hotmail.com>
2023-10-21 22:34:17 -0400

    LastVersionSamSoir

---
+++ Assets/Script/GoldDissapear.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldDissapear : MonoBehaviour
+{
+
+    public AudioSource audioPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            audioPlayer.Play();
+            other.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            other.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "CollisionTag") {
+           
+           
+        }
+            
+            
+    }
+}
