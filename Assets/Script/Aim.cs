e7b607ebef7227b2f4446e6310cf562b2f1f265f
Nementil <58092898+Nementil@users.noreply.github.com>
2023-10-21 21:18:55 -0400

    Aim+GameManager

--- Assets/Script/Aim.cs
+++ Assets/Script/Aim.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering.Universal;
 
 public class Aim : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     Vector2 mousePos;
     Rigidbody2D rb;
+    [SerializeField] float rangeLight = 5;
+    [SerializeField] float angleLight = 90;
+    [SerializeField] GameObject Lamp;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        Lamp = transform.GetChild(0).gameObject;
+        angleLight = Lamp.GetComponent<Light2D>().pointLightOuterAngle;
+        rangeLight = Lamp.GetComponent<Light2D>().pointLightOuterRadius;
     }
     private void Update()
     {
@@ -21,5 +27,31 @@ public class Aim : MonoBehaviour
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+        CheckEnemyInLight();
+
+        //Vector3 directionlooking= transform.rotation *new Vector3(0,1,0);
+        //Debug.DrawRay(transform.position, directionlooking, Color.red);
+    }
+    private void CheckEnemyInLight()
+    {
+        for(int angle=(int)Mathf.Floor(-angleLight/2); angle<angleLight/2;angle+=5)
+        {
+            Quaternion rotation = Quaternion.Euler(0, 0, angle+ transform.rotation.eulerAngles.z);
+            Vector3 direction= rotation*Vector3.up;
+            Ray ray = new(Lamp.transform.position, direction);
+            RaycastHit2D[] hit = Physics2D.RaycastAll(ray.origin, direction, rangeLight);
+
+            Debug.DrawRay(Lamp.transform.position, direction * 7, Color.red);
+            for (var i=0;i<hit.Length;i++)
+            {
+                Debug.Log(hit[i].collider.name);
+                if(hit[i].collider.gameObject.CompareTag("Enemy"))
+                {
+                    Debug.Log("Enemy!");
+                    //GameManager.Instance.enemiesKilled++;
+                    Destroy(hit[i].collider.gameObject);
+                }
+            }
+        }
     }
 }
---
+++ Assets/Script/EnemyAI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] SO_Enemy1 enemy;
+    [SerializeField] AI_State currentState;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        currentState = AI_State.Search;
+    }
+    private void Update()
+    {
+        if (currentState == AI_State.Search)
+        {
+            SearchPlayer();
+        }
+        if (currentState == AI_State.MoveToPlayer)
+        {
+            MoveToPlayer();
+        }
+    }
+    private void SearchPlayer()
+    {
+
+        Vector2 direction = (new Vector2(player.transform.position.x, player.transform.position.y) - new Vector2(this.transform.position.x, this.transform.position.y));
+        RaycastHit2D[] hit = Physics2D.CircleCastAll(transform.position, enemy.rangeLook, Vector2.zero);
+        foreach (RaycastHit2D gm in hit)
+        {
+            if (gm.collider.tag == player.tag)
+            {
+                currentState = AI_State.MoveToPlayer;
+            }
+        }
+    }
+    private void MoveToPlayer()
+    {
+        float speed = enemy.speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed);
+    }
+    enum AI_State
+    {
+        Search,
+        MoveToPlayer
+    }
+}
